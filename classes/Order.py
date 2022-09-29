677cd55e1d3fc55eb72b4adc3281d054dd9ac868
ArtemDaemon <artem.daemon.official@gmail.com>
2022-09-29 17:55:44 +0700

    Added encapsulation of Order class properties

--- classes/Order.py
+++ classes/Order.py
@@ -1,9 +1,9 @@
 class Order:
     def __init__(self, name, link, tags, price):
-        self.name = name
-        self.link = link
-        self.tags = tags
-        self.price = price
+        self.__name = name
+        self.__link = link
+        self.__tags = tags
+        self.__price = price
 
     def __str__(self):
-        return f"-{self.name}\n{self.tags}\n{self.price}\n{self.link}\n"
+        return f"-{self.__name}\n{self.__tags}\n{self.__price}\n{self.__link}\n"
