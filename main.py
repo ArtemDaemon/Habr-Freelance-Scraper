fa9cdee97f228386c381d1cc3b59d9a43ad66470
ArtemDaemon <artem.daemon.official@gmail.com>
2022-09-29 17:42:43 +0700

    Structurally divided the code. Made all HTML tags and classes constants

--- main.py
+++ main.py
@@ -2,29 +2,41 @@ import requests as req
 from bs4 import BeautifulSoup
 from classes.Order import Order
 
-URLForReq = "https://freelance.habr.com/tasks?q=python"
+URLForReq = "https://freelance.habr.com/tasks"
 URLForLink = "https://freelance.habr.com"
+
+TagArticle = "article"
+TagDiv = "div"
+TagA = "a"
+
+ClassArticle = "task task_list"
+ClassDivName = "task__title"
+ClassA = "task__item_link"
+ClassDivLink = "task__price"
+
+
+def load_orders():
+    loaded_orders = []
+    result = req.get(URLForReq)
+    soup = BeautifulSoup(result.text, "html.parser")
+    articles = soup.findAll(TagArticle, class_=ClassArticle)
+
+    for article in articles:
+        name_div = article.find(TagDiv, class_=ClassDivName)
+        name = name_div.text
+        link = URLForLink + name_div.find(TagA).get("href")
+        tags = []
+        link_tags = article.findAll(TagA, class_=ClassA)
+        for linkTag in link_tags:
+            tags.append(linkTag.text)
+
+        price = article.find(TagDiv, class_=ClassDivLink).text
+
+        loaded_orders.append(Order(name, link, tags, price))
+    return loaded_orders
 
 
 if __name__ == '__main__':
-    orders = []
-
-    result = req.get(URLForReq)
-    soup = BeautifulSoup(result.text, "html.parser")
-    articles = soup.findAll("article", class_="task task_list")
-
-    for article in articles:
-        nameDiv = article.find("div", class_="task__title")
-        name = nameDiv.text
-        link = URLForLink + nameDiv.find("a").get("href")
-        tags = []
-        linkTags = article.findAll("a", class_="tags__item_link")
-        for linkTag in linkTags:
-            tags.append(linkTag.text)
-
-        price = article.find("div", class_="task__price").text
-
-        orders.append(Order(name, link, tags, price))
-
+    orders = load_orders()
     for order in orders:
         print(order)
